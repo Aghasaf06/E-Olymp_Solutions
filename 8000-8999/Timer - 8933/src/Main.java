00052538ea946a7c58564f7bd40cd19cde821c91
Aghasaf06 <agasfm90@gmail.com>
2023-06-19 03:06:50 +0400

    Java | Problem - 8933

---
+++ 8000-8999/Timer - 8933/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        for(int i = n; 0 <= i; i--){
+            System.out.println(i + " sek");
+        }
+    }
+}
