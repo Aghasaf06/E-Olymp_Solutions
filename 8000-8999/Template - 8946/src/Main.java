7e2e08a90527773d8be27dc30ede71f271c26da8
Aghasaf06 <agasfm90@gmail.com>
2023-06-26 01:43:58 +0400

    Java | Problem - 8946

---
+++ 8000-8999/Template - 8946/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(j % 2 == i % 2){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
