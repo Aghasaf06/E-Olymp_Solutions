7e29e0c269e16b2ef079ab85198250de30d2d94b
Aghasaf06 <agasfm90@gmail.com>
2023-08-19 23:50:04 +0400

    Java | Problem - 8980

---
+++ 8000-8999/Number of sentences - 8980/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        char[] symbol = str.toCharArray();
+
+        int count = 0;
+        for(int i = 0; i < symbol.length; i++){
+            if(symbol[i] == '.' || symbol[i] == '!' || symbol[i] == '?'){
+                count++;
+                i += 2;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
